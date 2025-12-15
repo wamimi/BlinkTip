c4f8b7299e2b568975c4d55e1365c6b6bf18ea7e
wamimi <nellycyberpro@gmail.com>
2025-12-15 17:46:21 +0300

    feat: add VerificationResult interface

--- lib/wallet-verification.ts
+++ lib/wallet-verification.ts
@@ -6,3 +6,8 @@ import { verify } from '@noble/ed25519'
 import { PublicKey } from '@solana/web3.js'
 import bs58 from 'bs58'
 import { verifyMessage } from 'viem'
+
+interface VerificationResult {
+  valid: boolean
+  error?: string
+}
