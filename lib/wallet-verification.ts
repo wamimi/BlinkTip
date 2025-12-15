bfbec19f526b991040bcbfb5622862062d757c82
wamimi <nellycyberpro@gmail.com>
2025-12-15 17:45:39 +0300

    feat: create wallet verification utility file

---
+++ lib/wallet-verification.ts
@@ -0,0 +1,8 @@
+/**
+ * Wallet ownership verification via signature
+ */
+
+import { verify } from '@noble/ed25519'
+import { PublicKey } from '@solana/web3.js'
+import bs58 from 'bs58'
+import { verifyMessage } from 'viem'
