aac0a14ef02768d74f2360e37098ac0c72dac2f5
wamimi <nellycyberpro@gmail.com>
2025-12-19 00:01:40 +0300

    disables optimizedCss

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 import webpack from 'webpack'
 
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizeCss: false,
+  },
   images: {
     remotePatterns: [
       {
